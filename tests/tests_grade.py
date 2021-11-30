a3e06d969813342746133522e90e8c8e731a304d
tusiaa <sandra.leman146@gmail.com>
2021-11-30 18:08:23 +0100

    TDD - Init tests

--- tests/tests_grade.py
+++ tests/tests_grade.py
@@ -3,22 +3,40 @@ from src.grade import *
 
 class PasswordTest(unittest.TestCase):
 
-    def test_register_init(self):
+    def test_grade_init(self):
         self.temp = grade(5, 5)
         self.assertNotEqual(self.temp, None)
 
-    def test_register_init_grade_to_big(self):
+    def test_grade_init_grade_to_big(self):
         with self.assertRaises(ValueError):
             grade(10, 5)
 
-    def test_register_init_grade_to_small(self):
+    def test_grade_init_grade_to_small(self):
         with self.assertRaises(ValueError):
             grade(-1, 5)
 
-    def test_register_init_scale_to_big(self):
+    def test_grade_init_scale_to_big(self):
         with self.assertRaises(ValueError):
             grade(5, 20)
 
-    def test_register_init_scale_to_small(self):
+    def test_grade_init_scale_to_small(self):
         with self.assertRaises(ValueError):
             grade(5, -1)
+
+    def test_grade_init_grade_to_big_scale_to_big(self):
+        with self.assertRaises(ValueError):
+            grade(10, 20)
+
+    def test_grade_init_grade_to_small_scale_to_small(self):
+        with self.assertRaises(ValueError):
+            grade(-1, -1)
+
+    def test_grade_init_grade_to_big_scale_to_small(self):
+        with self.assertRaises(ValueError):
+            grade(10, -1)
+
+    def test_grade_init_grade_to_small_scale_to_big(self):
+        with self.assertRaises(ValueError):
+            grade(-1, 20)
+
+    
