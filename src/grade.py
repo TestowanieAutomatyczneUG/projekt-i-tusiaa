a5abcaf1a47bdac54857099ba7a8667789b7f8e1
tusiaa <sandra.leman146@gmail.com>
2021-11-30 17:59:12 +0100

    TDD - To big grade error

--- src/grade.py
+++ src/grade.py
@@ -1,4 +1,6 @@
 class grade:
     def __init__(self, grade: int, scale: int) -> None:
+        if(grade > 6):
+            raise ValueError("Grade cannot be greater than 6")
         self.grade = grade
         self.scale = scale
