a2f63b0ec955e2562fa58909b3cc2c11836a19c8
tusiaa <sandra.leman146@gmail.com>
2021-11-30 18:02:37 +0100

    TDD - scale to big error

--- src/grade.py
+++ src/grade.py
@@ -4,5 +4,7 @@ class grade:
             raise ValueError("Grade cannot be greater than 6")
         if(grade < 1):
             raise ValueError("Grade cannot be less than 1")
+        if(scale > 10):
+            raise ValueError("Scale cannot be greater than 10")
         self.grade = grade
         self.scale = scale
