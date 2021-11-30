3b19de7632f4f2768d3c52714a772c9ed7b4864a
tusiaa <sandra.leman146@gmail.com>
2021-11-30 18:00:08 +0100

    TDD - Grade to small test

--- tests/tests_grade.py
+++ tests/tests_grade.py
@@ -10,3 +10,7 @@ class PasswordTest(unittest.TestCase):
     def test_register_init_grade_to_big(self):
         with self.assertRaises(ValueError):
             grade(10, 5)
+
+    def test_register_init_grade_to_small(self):
+        with self.assertRaises(ValueError):
+            grade(-1, 5)
