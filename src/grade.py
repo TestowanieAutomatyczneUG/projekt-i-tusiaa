e860da121f9276f8e897d88884fdf4f5db867799
tusiaa <sandra.leman146@gmail.com>
2021-11-30 21:34:47 +0100

    TDD - Grade init number string update

--- src/grade.py
+++ src/grade.py
@@ -1,9 +1,13 @@
 class grade:
     def __init__(self, grade: int, scale: int) -> None:
         if type(grade) is not int:
-            raise ValueError("Grade must be an integer")
+            if not (type(grade) is str and grade.isdigit() is True):
+                raise ValueError("Grade must be an integer")
         if type(scale) is not int:
-            raise ValueError("Scale must be an integer")
+            if not (type(scale) is str and scale.isdigit() is True):
+                raise ValueError("Scale must be an integer")
+        grade = int(grade)
+        scale = int(scale)
         if(grade > 6):
             raise ValueError("Grade cannot be greater than 6")
         if(grade < 1):
