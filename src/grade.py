b58707a9f59286a0a3e0ecf10aa3bda1095ec4f5
tusiaa <sandra.leman146@gmail.com>
2021-11-30 18:00:58 +0100

    TDD - Grade to small error

--- src/grade.py
+++ src/grade.py
@@ -2,5 +2,7 @@ class grade:
     def __init__(self, grade: int, scale: int) -> None:
         if(grade > 6):
             raise ValueError("Grade cannot be greater than 6")
+        if(grade < 1):
+            raise ValueError("Grade cannot be less than 1")
         self.grade = grade
         self.scale = scale
