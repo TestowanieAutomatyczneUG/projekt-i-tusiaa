7e19f2c4eb54fa756af30870ace12940d80e17e5
tusiaa <sandra.leman146@gmail.com>
2021-12-05 23:33:13 +0100

    Added pesel

--- src/student.py
+++ src/student.py
@@ -1,9 +1,10 @@
 from src.subject import *
 
 class student:
-    def __init__(self, name: str, surname: str) -> None:
+    def __init__(self, name: str, surname: str, pesel: int) -> None:
         self.subjects = []
         self.remarks = []
         self.name = name
         self.surname = surname
+        self.pesel = pesel
         
