58b325ed97663bfde7dfd3f0b5da7b62b09298b8
tusiaa <sandra.leman146@gmail.com>
2021-12-07 20:58:03 +0100

    TDD - Student tests init

---
+++ tests/tests_student.py
@@ -0,0 +1,50 @@
+import unittest
+from parameterized import parameterized_class, parameterized
+from hamcrest import  *
+from src.student import *
+from src.Pesel_matcher import *
+
+@parameterized_class(('value', 'error'), [
+    (1, ValueError),
+    (1.5, ValueError),
+    (True, ValueError),
+    (None, ValueError),
+    ("", ValueError),
+    ([1,2,3], ValueError),
+    ({'name': 2, 'grades': 4}, ValueError),
+])
+class StudentParamerizedTest1(unittest.TestCase):
+    def test_subject_init_wrong_name(self):
+        assert_that(calling(student).with_args(self.value, "Kowalski", "96032687885"), raises(self.error))
+
+    def test_subject_init_wrong_surname(self):
+        assert_that(calling(student).with_args("Jan", self.value, "96032687885"), raises(self.error))
+
+
+class StudentTest(unittest.TestCase):
+    def setUp(self):
+        self.temp = student("Jan", "Kowalski", "96032687885")
+
+    def test_student_init(self):
+        assert_that(self.temp, not_none())
+
+    @parameterized.expand([
+        ("", ValueError),
+        ("123456789", ValueError),
+        ("96032687886", ValueError),
+        ("12345678910", ValueError),
+        (96032687886, ValueError),
+        (9603.2687885, ValueError),
+        (True, ValueError),
+        (None, ValueError),
+        ([1,2,3], ValueError),
+        ({'name': 2, 'grades': 4}, ValueError),
+    ])
+    def test_student_init_wrong_pesel(self, pesel, error):
+        assert_that(calling(student).with_args("Jan", "Kowalski", pesel), raises(error))
+
+
+
+    
+    def tearDown(self):
+        del self.temp
