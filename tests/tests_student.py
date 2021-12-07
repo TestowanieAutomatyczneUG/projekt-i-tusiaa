f37a63719134698b8087ec542eaff03811e842e6
tusiaa <sandra.leman146@gmail.com>
2021-12-07 21:09:43 +0100

    TDD - Student tests getters

--- tests/tests_student.py
+++ tests/tests_student.py
@@ -43,6 +43,21 @@ class StudentTest(unittest.TestCase):
     def test_student_init_wrong_pesel(self, pesel, error):
         assert_that(calling(student).with_args("Jan", "Kowalski", pesel), raises(error))
 
+    def test_student_get_name(self):
+        assert_that(self.temp.get_name(), equal_to("Jan"))
+
+    def test_student_get_surname(self):
+        assert_that(self.temp.get_surname(), equal_to("Kowalski"))
+
+    def test_student_get_pesel(self):
+        assert_that(self.temp.get_pesel(), is_(IsValidPesel()))
+
+    def test_student_get_remarks(self):
+        assert_that(self.temp.get_remarks(), instance_of(list))
+
+    def test_student_get_subjects(self):
+        assert_that(self.temp.get_subjects(), instance_of(list))
+
 
 
     
