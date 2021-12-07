0a6bec7b90fb583ad233116cb1f7751cf540117c
tusiaa <sandra.leman146@gmail.com>
2021-12-07 03:58:59 +0100

    Pesel matcher

---
+++ src/Pesel_matcher.py
@@ -0,0 +1,28 @@
+def pesel(pesel: str):
+    if not isinstance(pesel, str):
+        return False
+    if len(pesel) != 11 or not pesel.isdigit():
+        return False
+
+    wagi = [1, 3, 7, 9]
+    value = 0
+    months = list(range(81, 93)) + list(range(1, 13)) + list(range(21, 33))    
+
+    for i in range(0, 10):
+        value += int(pesel[i]) * wagi[i % 4]
+
+    value = value % 10
+
+    if value != 0:
+        value = 10 - value
+
+    if value != int(pesel[10]):
+        return False
+
+    if int(pesel[2:4]) not in months:
+        return False
+
+    if int(pesel[4:6]) not in range(1, 32):
+        return False
+
+    return True
