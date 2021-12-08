2dd17c9532784fbe7ed222ef1266ab502f2f82bb
tusiaa <sandra.leman146@gmail.com>
2021-12-09 00:51:08 +0100

    Changes in function names

--- tests/tests_register.py
+++ tests/tests_register.py
@@ -23,16 +23,16 @@ class RegisterParamerizedTest1(unittest.TestCase):
         assert_that(self.temp.add_student).raises(self.error).when_called_with("Jan", self.value, "96032687885")
 
     def test_register_find_students_by_name_wrong(self):
-        assert_that(self.temp.find_students_by_name).raises(self.error).when_called_with(self.value)
+        assert_that(self.temp.find_by_name).raises(self.error).when_called_with(self.value)
 
     def test_register_find_students_by_surname_wrong(self):
-        assert_that(self.temp.find_students_by_surname).raises(self.error).when_called_with(self.value)
+        assert_that(self.temp.find_by_surname).raises(self.error).when_called_with(self.value)
 
     def test_register_find_students_by_name_and_surname_wrong_name(self):
-        assert_that(self.temp.find_students_by_name_and_surname).raises(self.error).when_called_with(self.value, "Kowalski")
+        assert_that(self.temp.find_by_name_and_surname).raises(self.error).when_called_with(self.value, "Kowalski")
 
     def test_register_find_students_by_name_and_surname_wrong_surname(self):
-        assert_that(self.temp.find_students_by_name_and_surname).raises(self.error).when_called_with("Jan", self.value)
+        assert_that(self.temp.find_by_name_and_surname).raises(self.error).when_called_with("Jan", self.value)
 
     def test_register_change_student_name_wrong_name(self):
         assert_that(self.temp.change_student_name).raises(self.error).when_called_with("96032687885", self.value)
@@ -67,7 +67,7 @@ class RegisterParamerizedTest2(unittest.TestCase):
         assert_that(self.temp.add_student).raises(self.error).when_called_with(self.value)
 
     def test_register_find_student_by_pesel_wrong(self):
-        assert_that(self.temp.find_student_by_pesel).raises(self.error).when_called_with(self.value)
+        assert_that(self.temp.find_by_pesel).raises(self.error).when_called_with(self.value)
 
     def test_register_delete_student_wrong(self):
         assert_that(self.temp.delete_student).raises(self.error).when_called_with(self.value)
@@ -110,11 +110,11 @@ class TestRegister(unittest.TestCase):
     def test_register_delete_student_by_pesel(self):
         self.temp.add_student("Jan", "Kowalski", "94071449639")
         self.temp.delete_student("94071449639")
-        assert_that(self.temp.find_student_by_pesel("94071449639")).is_none()
+        assert_that(self.temp.find_by_pesel("94071449639")).is_none()
 
     def test_register_delete_student_by_pesel_not_found(self):
         self.temp.delete_student("94071449639")
-        assert_that(self.temp.find_student_by_pesel("94071449639")).is_none()
+        assert_that(self.temp.find_by_pesel("94071449639")).is_none()
 
     def test_register_delete_student_by_object(self):
         Student = student("Jan", "Kowalski", "96032687885")
@@ -134,63 +134,63 @@ class TestRegister(unittest.TestCase):
 
     def test_register_find_student_by_pesel(self):
         self.temp.add_student("Jan", "Kowalski", "96032687885")
-        assert_that(self.temp.find_student_by_pesel("96032687885")).is_instance_of(student)
+        assert_that(self.temp.find_by_pesel("96032687885")).is_instance_of(student)
 
     def test_register_find_student_by_pesel_not_found(self):
-        assert_that(self.temp.find_student_by_pesel("94071449639")).is_none()
+        assert_that(self.temp.find_by_pesel("94071449639")).is_none()
 
     def test_register_find_students_by_name(self):
         self.temp.add_student("Jan", "Kowalski", "96032687885")
         self.temp.add_student("Jan", "Nowak", "03241311845")
         self.temp.add_student("Jan", "Konkol", "94071449639")
-        assert_that(self.temp.find_students_by_name("Jan")).is_length(3)
+        assert_that(self.temp.find_by_name("Jan")).is_length(3)
 
     def test_register_find_students_by_name_not_found(self):
-        assert_that(self.temp.find_students_by_name("Jan")).is_empty()
+        assert_that(self.temp.find_by_name("Jan")).is_empty()
 
     def test_register_find_students_by_surname(self):
         self.temp.add_student("Jan", "Kowalski", "96032687885")
         self.temp.add_student("Ania", "Kowalski", "03241311845")
         self.temp.add_student("Karol", "Kowalski", "94071449639")
-        assert_that(self.temp.find_students_by_surname("Kowalski")).is_length(3)
+        assert_that(self.temp.find_by_surname("Kowalski")).is_length(3)
 
     def test_register_find_students_by_surname_not_found(self):
-        assert_that(self.temp.find_students_by_surname("Kowalski")).is_empty()
+        assert_that(self.temp.find_by_surname("Kowalski")).is_empty()
 
     def test_register_find_students_by_name_and_surname(self):
         self.temp.add_student("Jan", "Kowalski", "96032687885")
         self.temp.add_student("Jan", "Kowalski", "03241311845")
-        assert_that(self.temp.find_students_by_name_and_surname("Jan", "Kowalski")).is_length(2)
+        assert_that(self.temp.find_by_name_and_surname("Jan", "Kowalski")).is_length(2)
 
     def test_register_find_students_by_name_and_surname_not_found(self):
-        assert_that(self.temp.find_students_by_name_and_surname("Jan", "Kowalski")).is_empty()
+        assert_that(self.temp.find_by_name_and_surname("Jan", "Kowalski")).is_empty()
 
     def test_register_change_student_name(self):
         self.temp.add_student("Jan", "Kowalski", "96032687885")
         self.temp.change_student_name("96032687885", "Karol")
-        assert_that(self.temp.find_student_by_pesel("96032687885").get_name()).is_equal_to("Karol")
+        assert_that(self.temp.find_by_pesel("96032687885").get_name()).is_equal_to("Karol")
 
     def test_register_change_student_name_not_found(self):
         self.temp.change_student_name("96032687885", "Karol")
-        assert_that(self.temp.find_student_by_pesel("96032687885")).is_none()
+        assert_that(self.temp.find_by_pesel("96032687885")).is_none()
 
     def test_register_change_student_surname(self):
         self.temp.add_student("Jan", "Kowalski", "96032687885")
         self.temp.change_student_surname("96032687885", "Nowak")
-        assert_that(self.temp.find_student_by_pesel("96032687885").get_surname()).is_equal_to("Nowak")
+        assert_that(self.temp.find_by_pesel("96032687885").get_surname()).is_equal_to("Nowak")
 
     def test_register_change_student_surname_not_found(self):
         self.temp.change_student_surname("96032687885", "Nowak")
-        assert_that(self.temp.find_student_by_pesel("96032687885")).is_none()
+        assert_that(self.temp.find_by_pesel("96032687885")).is_none()
 
     def test_register_change_student_pesel(self):
         self.temp.add_student("Jan", "Kowalski", "96032687885")
         self.temp.change_student_pesel("96032687885", "03241311845")
-        assert_that(self.temp.find_student_by_pesel("03241311845")).is_not_none()
+        assert_that(self.temp.find_by_pesel("03241311845")).is_not_none()
 
     def test_register_change_student_pesel_not_found(self):
         self.temp.change_student_pesel("96032687885", "03241311845")
-        assert_that(self.temp.find_student_by_pesel("03241311845")).is_none()
+        assert_that(self.temp.find_by_pesel("03241311845")).is_none()
 
     def test_register_change_student_pesel_already_exists(self):
         self.temp.add_student("Jan", "Kowalski", "96032687885")
