b27e36d0982f26695c95d0004b74a8626615d733
tusiaa <sandra.leman146@gmail.com>
2021-12-08 01:43:52 +0100

    TDD - Student init errors

--- src/student.py
+++ src/student.py
@@ -1,10 +1,21 @@
 from src.subject import *
+from src.Pesel_matcher import pesel
 
 class student:
-    def __init__(self, name: str, surname: str, pesel: int) -> None:
-        self.subjects = []
-        self.remarks = []
+    def __init__(self, name: str, surname: str, Pesel: str) -> None:
+        if type(Pesel) is int:
+            Pesel = str(Pesel)
+        if not pesel(Pesel):
+            raise ValueError("PESEL musi być poprawny")
+        if not name or type(name) != str:
+            raise ValueError("Imię musi być napisem")
+        if not surname or type(surname) != str:
+            raise ValueError("Nazwisko musi być napisem")
+
         self.name = name
         self.surname = surname
-        self.pesel = pesel
+        self.pesel = Pesel
+        self.subjects = []
+        self.remarks = []
+        
         
