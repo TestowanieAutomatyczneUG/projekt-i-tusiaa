04a03d383fba22ea79cc6c1b445a6422ee3a9ea7
tusiaa <sandra.leman146@gmail.com>
2021-12-08 05:45:10 +0100

    Added DocTest

--- src/Pesel_matcher.py
+++ src/Pesel_matcher.py
@@ -1,6 +1,41 @@
 from hamcrest.core.base_matcher import BaseMatcher
+import doctest
 
 def pesel(pesel: str):
+
+    r"""Checks if pesel is valid.
+        >>> pesel("96032687885")
+        True
+        >>> pesel("94071449639")
+        True
+        >>> pesel("03241311845")
+        True
+        >>> pesel("12345678901")
+        False
+        >>> pesel("")
+        False
+        >>> pesel("123456789")
+        False
+        >>> pesel("96032687886")
+        False
+        >>> pesel("96033687884")
+        False
+        >>> pesel("12345678910")
+        False
+        >>> pesel(96032687886)
+        False
+        >>> pesel(9603.2687885)
+        False
+        >>> pesel(True)
+        False
+        >>> pesel(None)
+        False
+        >>> pesel([1,2,3])
+        False
+        >>> pesel({'name': 2, 'grades': 4})
+        False
+        """
+
     if not isinstance(pesel, str):
         return False
     if len(pesel) != 11 or not pesel.isdigit():
@@ -30,6 +65,9 @@ def pesel(pesel: str):
     return True
 
 class IsValidPesel(BaseMatcher):
+    def __init__(self):
+        doctest.testmod()
+
     def _matches(self, item):
         return pesel(item)
 
