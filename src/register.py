69416e04dea648c774c136e5f674189ac2d00559
tusiaa <sandra.leman146@gmail.com>
2021-12-16 02:18:54 +0100

    TDD - Register import and export functions

--- src/register.py
+++ src/register.py
@@ -1,4 +1,5 @@
 from src.student import *
+import csv
 
 class register:
     def __init__(self):
@@ -88,3 +89,71 @@ class register:
             raise ValueError("Student already exists")
         if self.find_by_pesel(old_pesel):
             self.find_by_pesel(old_pesel).set_pesel(new_pesel)
+
+    def import_students(self, file: str):
+        if not file or type(file) is not str:
+            raise ValueError("Invalid file path")
+        try:
+            with open(file, "r") as csv_file:
+                reader = csv.reader(csv_file, delimiter=",")
+                headers = next(reader)
+                for row in reader:
+                    for i in range(0, len(row)):
+                        row[i] = row[i].strip(" ")
+                    if not self.find_by_pesel(row[0]):
+                        self.add_student(row[1], row[2], row[0])
+                    if len(row) > 3:
+                        if not self.find_by_pesel(row[0]).find_subject(row[3]):
+                            self.find_by_pesel(row[0]).add_subject(row[3])
+                    if len(row) > 4:
+                        self.find_by_pesel(row[0]).find_subject(row[3]).add_grade(grade(row[4], row[5]))
+        except FileNotFoundError:
+            raise FileNotFoundError("File not found")
+
+    def export_students(self, file: str):
+        if not file or type(file) is not str:
+            raise ValueError("Invalid file path")
+        try:
+            with open(file, "w") as csv_file:
+                writer = csv.writer(csv_file, delimiter=",")
+                writer.writerow(["Pesel", "Name", "Surname", "Subject", "Grade", "Scale"])
+                for student in self.students:
+                    if student.get_subjects():
+                        for subject in student.get_subjects():
+                            if subject.get_grades():
+                                for grade in subject.get_grades():
+                                    writer.writerow([student.pesel, student.name, student.surname, subject.name, grade.grade, grade.scale])
+                            else:
+                                writer.writerow([student.pesel, student.name, student.surname, subject.name])
+                    else:
+                        writer.writerow([student.pesel, student.name, student.surname])
+        except FileNotFoundError:
+            raise FileNotFoundError("File not found")
+
+    def import_remarks(self, file):
+        if not file or type(file) is not str:
+            raise ValueError("Invalid file path")
+        try:
+            with open(file, "r") as csv_file:
+                reader = csv.reader(csv_file, delimiter=";")
+                headers = next(reader)
+                for row in reader:
+                    if not self.find_by_pesel(row[0]):
+                        raise ValueError("Student not found")
+                    self.find_by_pesel(row[0]).add_remark(row[1])
+        except FileNotFoundError:
+            raise FileNotFoundError("File not found")
+
+    def export_remarks(self, file):
+        if not file or type(file) is not str:
+            raise ValueError("Invalid file path")
+        try:
+            with open(file, "w") as csv_file:
+                writer = csv.writer(csv_file, delimiter=";")
+                writer.writerow(["Pesel", "Remark"])
+                for student in self.students:
+                    if student.get_remarks():
+                        for remark in student.get_remarks():
+                            writer.writerow([student.pesel, remark])
+        except FileNotFoundError:
+            raise FileNotFoundError("File not found")
